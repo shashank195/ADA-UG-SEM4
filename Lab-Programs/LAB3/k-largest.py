ee903a9875dad5ebee21d094685033e79d8bf4b7
Shashank Kumar <shashank.cs18@bmsce.ac.in>
2020-02-19 11:31:32 +0530

    Update k-largest.py

--- Lab-Programs/LAB3/k-largest.py
+++ Lab-Programs/LAB3/k-largest.py
@@ -1,4 +1,4 @@
-def kthSmallest(arr, n, k): 
+def kthLargest(arr, n, k): 
       arr.sort() 
     return arr[k-1] 
   
@@ -6,5 +6,5 @@ if __name__=='__main__':
     arr = [12, 3, 5, 7, 19] 
     n = len(arr) 
     k = 2
-    print("K'th smallest element is", 
-          kthSmallest(arr, n, k)) 
+    print("K'th largest element is", 
+          kthLargest(arr, n, k)) 
