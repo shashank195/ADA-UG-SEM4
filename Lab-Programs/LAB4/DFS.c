d0ab0bfaf60439010bed5d5a99669f5c8a5ac31c
Shashank Kumar <shashank.cs18@bmsce.ac.in>
2020-03-11 11:56:51 +0530

    Create DFS.c

---
+++ Lab-Programs/LAB4/DFS.c
@@ -0,0 +1,36 @@
+#include<stdio.h>                                                 //DFS Implementation by C
+#include<conio.h>
+int a[20][20],reach[20],n;
+void dfs(int v){
+     int i;
+     reach[v]=1;
+     for(i=1;i<=n;i++)
+         if(a[v][i]&&!reach[i]){
+            printf(“\n%d->%d”,v,i);
+            dfs(i);
+            }
+ }
+ int main(){
+    int i,j,count=0;
+    printf(“\nEnter no of vertices : “);
+    scanf(“%d”,&n);
+    for(i=1;i<=n;i++)
+        for(j=1;j<=n;j++){
+        reach[i]=0;
+        a[i][j]=0;
+        }
+    printf(“\nEnter adjacency matrix : \n”);
+    for(i=1;i<=n;i++)
+      for(j=1;j<=n;j++)
+        scanf(“%d”,&a[i][j]);
+    dfs(1);
+    for(i=1;i<=n;i++)
+       if(reach[i])
+         count++;
+    if(count==n)
+       printf(“\nGraph is connected.”);
+    else
+       printf(“\nGraph is disconnected.”);
+    getch();
+    return(0);
+  }
