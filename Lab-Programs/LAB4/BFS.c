ecb6efcbae8865af3ba543877ee8c67de4509432
Shashank Kumar <shashank.cs18@bmsce.ac.in>
2020-03-11 11:55:29 +0530

    Create BFS.c

---
+++ Lab-Programs/LAB4/BFS.c
@@ -0,0 +1,41 @@
+#include<stdio.h>                                                         /*Implementation of BFS Algorithm using C*/
+#include<conio.h> 
+int a[20][20],q[20],visited[20],r=-1,f=0,i,j,n; 
+void bfs(int v); 
+int main(){ 
+    int v; 
+       printf(“\nEnter no. of vertices:”); 
+       scanf(“%d”,&n); 
+       for(i=1;i<=n;i++) 
+        { 
+         q[i]=0; 
+         visited[i]=0; 
+        } 
+       printf(“\nEnter Adjacency Matrix of the graph:”); 
+         for(i=1;i<=n;i++){ 
+            for(j=1;j<=n;j++){ 
+              printf(“nEnter a[%d][%d]:”,i,j); 
+              scanf(“%d”,&a[i][j]); 
+              } 
+            } 
+         printf(“\nEnter the node of ur Choice:”); 
+         scanf(“%d”,&v); 
+         bfs(v); 
+         printf(“\nAll the nodes reachable from node->%d is/are : nn”,v); 
+         for(i=1;i<=n;i++){ 
+             if(visited[i]) 
+                printf(“%dt”,i); 
+                } 
+         getch(); 
+         return(0); 
+    } 
+    
+    void bfs(int v){ 
+         for(i=1;i<=n;i++) 
+                if(a[v][i] && !visited[i]) 
+                    q[++r]=i; 
+                if(f<=r){ 
+                 visited[q[f]]=1; 
+                 bfs(q[f++]); 
+                 }  
+         }
